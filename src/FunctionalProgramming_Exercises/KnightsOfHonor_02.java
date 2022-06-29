4089379da148f2964fa1ff4a754d2b61270037f4
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-06-29 09:11:07 +0200

    Add files via upload

---
+++ src/FunctionalProgramming_Exercises/KnightsOfHonor_02.java
@@ -0,0 +1,23 @@
+package FunctionalProgramming_Exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class KnightsOfHonor_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Function<приема, връща> -> apply
+        //Consumer<приема> -> void -> accept
+        //Supplier<връща> -> get
+        //Predicate<приема> -> връща true / false -> test
+        //BiFunction<приема1, приема2, връща> -> apply
+
+        String[] names = scanner.nextLine().split("\\s+");
+        Consumer<String> getSir = name -> System.out.println("Sir "+name);
+
+        Arrays.stream(names)
+                .forEach(getSir);
+    }
+}
